8904881406a23c0203f66babb22d83be8d75ce41
Salman-S7 <sammyzed1505@gmail.com>
2024-04-10 12:31:06 +0530

    fixed error

--- client/attendence-sys/src/components/StartBreakButton.jsx
+++ client/attendence-sys/src/components/StartBreakButton.jsx
@@ -48,7 +48,7 @@ function StartBreakButton({name}) {
       } catch (error) {
         console.error('Error during starting break:', error);
         if(error.response.status === 400){
-           
+          const errorMessege = error.response.data.message;
           alert(errorMessege);
         }
         // Handle error, display a message to the user, etc.
